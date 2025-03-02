9bbc67f01c815924532a97e6cd08b214346a57f0
Carva70 <nachocargar@gmail.com>
2025-03-02 23:33:16 +0100

    fix:rohrflote_animation

--- src/main/java/com/finchy/pipeorgans/event/ModEventBusClientEvents.java
+++ src/main/java/com/finchy/pipeorgans/event/ModEventBusClientEvents.java
@@ -9,6 +9,7 @@ import com.finchy.pipeorgans.block.pipes.piccolo.PiccoloRenderer;
 import com.finchy.pipeorgans.block.pipes.subbass.SubbassRenderer;
 import com.finchy.pipeorgans.block.pipes.trompette.TrompetteRenderer;
 import com.finchy.pipeorgans.block.pipes.englishhorn.EnglishHornRenderer;
+import com.finchy.pipeorgans.block.pipes.rohrflote.RohrfloteRenderer;
 import com.finchy.pipeorgans.init.AllBlockEntities;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -28,5 +29,6 @@ public class ModEventBusClientEvents {
         event.registerBlockEntityRenderer(AllBlockEntities.TROMPETTE_BLOCK_ENTITY.get(), TrompetteRenderer::new);
         event.registerBlockEntityRenderer(AllBlockEntities.NASARD_BLOCK_ENTITY.get(), NasardRenderer::new);
         event.registerBlockEntityRenderer(AllBlockEntities.ENGLISHHORN_BLOCK_ENTITY.get(), EnglishHornRenderer::new);
+        event.registerBlockEntityRenderer(AllBlockEntities.ROHRFLOTE_BLOCK_ENTITY.get(), RohrfloteRenderer::new);
     }
 }
